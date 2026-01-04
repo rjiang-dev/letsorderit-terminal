8d776e584a910f5c9ae3c33003b7363935518f6d
Rani Jiang <rjiang.dev@gmail.com>
2026-01-04 19:40:07 +1100

    0.0.1.pre-release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 21
         targetSdk = 36
         versionCode = 1
-        versionName = "0.0.0"
+        versionName = "0.0.1.pre-release"
         buildConfigField("int", "MIN_SDK_VERSION", "$minSdk")
     }
 
